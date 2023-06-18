5d5ae2da965d36c2dd1aff456fff441f0ef9b47a
Pranit Brahmbhatt <pranit@esamudaay.com>
2023-06-17 22:46:59 -0400

    login and registration screens done

--- src/user/login.jsx
+++ src/user/login.jsx
@@ -1,13 +1,17 @@
 export const Login = () => {
     return(
         <form>
-            <label for="username">Username</label>
+            <h1>Welcome to MovieFlix!</h1>
+            <h2>It's great having you back! Let's binge!</h2>
+            <h3>Login Here</h3>
+            <label style={{marginRight: "1%"}} for="username">Username</label>
             <input type="text" placeholder="username123" 
-                id="login_uname" name="username"></input>
-            <br></br>
-            <label for="password">Password</label>
+                id="login_uname" name="username"></input><br/><br/>
+            <label style={{marginRight: "1%"}} for="password">Password</label>
             <input type="password" placeholder="*****" 
-                id="login_pword" name="password"></input>
+                id="login_pword" name="password"></input><br/><br/>
+            <button type="submit">Login</button>
+            <p>Don't have an account? <button>Register Here</button></p>
         </form>
     )
 }
